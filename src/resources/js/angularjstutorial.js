6de59756fd120446a72361a77006520699a3c72c
subbucn.it@gmail.com <$Lnmurthy18>
2015-09-24 19:22:54 -0700

    default controller values

--- src/resources/js/angularjstutorial.js
+++ src/resources/js/angularjstutorial.js
@@ -1,8 +1,7 @@
-/* use strict */
-var app = angular.module('MyApp', []);
-app.controller("MainCtrl", function ($scope)
-{
-	$scope.data = {
-		label: "My Button 2"
-	};
+var app=angular.module('MyApp',[]);
+app.controller("MainCtrl",function($scope){
+$scope.data={
+    label: "SChaturvedula",
+    class: "Success"
+};
 });
